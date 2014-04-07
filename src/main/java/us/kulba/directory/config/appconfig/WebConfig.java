b932304bd2877794294f9c2c954be19f74e40245
James Kulba <jjkulba@yahoo.com>
2014-04-06 22:13:00 -0700

    + udpates for security

--- src/main/java/us/kulba/directory/config/appconfig/WebConfig.java
+++ src/main/java/us/kulba/directory/config/appconfig/WebConfig.java
@@ -1,10 +1,16 @@
 package us.kulba.directory.config.appconfig;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "us.kulba.directory.*")
 public class WebConfig extends WebMvcConfigurerAdapter {
     /**
      * Create request mapping bean for @RequestMapping
--- src/main/java/us/kulba/directory/config/securityconfig/WebSecurityConfig.java
+++ src/main/java/us/kulba/directory/config/securityconfig/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package us.kulba.directory.config.securityconfig;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -17,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .withUser("admin")
                 .password("admin")
-                .roles("ADMIN","USER");
+                .roles("ADMIN", "USER");
     }
 
     @Override
@@ -30,21 +34,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/views/**");
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/login","/login/form**","/register","/logout").permitAll()
-            .antMatchers("/admin","/admin/**").hasRole("ADMIN")
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginPage("/login/form")
-            .loginProcessingUrl("/login")
-            .failureUrl("/login/form?error")
-            .permitAll();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http
+//            .csrf().disable()
+//            .authorizeRequests()
+//            .antMatchers("/login", "/login/form**", "/register", "/logout").permitAll()
+//            .antMatchers("/admin", "/admin/**").hasRole("ADMIN")
+//            .anyRequest().authenticated()
+//            .and()
+//            .formLogin()
+//            .loginPage("/login/form")
+//            .loginProcessingUrl("/login")
+//            .failureUrl("/login/form?error")
+//            .permitAll();
+//    }
 
 }
